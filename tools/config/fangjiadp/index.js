2f91084f7c12e9b36ddb14e444fd33dc2bc20214
nick <nick121212@126.com>
2016-08-10 16:04:25 +0800

    房点评

---
+++ tools/config/fangjiadp/index.js
@@ -0,0 +1,31 @@
+// http://www.fangjiadp.com/shanghai/newhouse/loudetail/658917
+
+/**
+ * Created by NICK on 16/7/1.
+ */
+
+let _ = require("lodash");
+
+module.exports = exports = (core) => {
+    let config = new core.utils.builder("fangjiadp", "www.fangjiadp.com", []);
+
+    config.setBaseInfo(1000, "superagent");
+    config.initDomain = "www.fangjiadp.com/shanghai/newhouse/index";
+    config.proxySettings = {
+        useProxy: false,
+        charset: "utf-8",
+        timeout: 5000
+    };
+    // 白名单
+    // 二手房正则
+    config.addWhitePath(/^\/shanghai\/newhouse\/(?:index|detail\/\d+|loudetail\/\d+)\/?$/);
+
+    // -----------------页面配置------------------
+    _.forEach(core.config.fangjiadp.pages, (page) => {
+        if (typeof page === "function") {
+            page(config);
+        }
+    });
+
+    return config;
+};
