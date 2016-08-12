9a8cd9d0aa05ff4cbf4b3e3376fede320444d8b0
nick <nick121212@126.com>
2016-08-12 15:46:58 +0800

    房点评

--- index.js
+++ index.js
@@ -20,34 +20,3 @@ consign({verbose: false})
     .into(app, core);
 
 console.log(`pid:${process.pid};ENV:${process.env.ENV}`);
-
-//递归创建目录 异步方法
-function mkdirs(dirname, mode, callback){
-    fs.exists(dirname, function (exists){
-        if(exists){
-            callback();
-        }else{
-            console.log(path.dirname(dirname));
-            mkdirs(path.dirname(dirname), mode, function (){
-                fs.mkdir(dirname, mode, callback);
-            });
-        }
-    });
-}
-//递归创建目录 同步方法
-function mkdirsSync(dirname, mode){
-    console.log(dirname);
-    if(fs.existsSync(dirname)){
-        return true;
-    }else{
-        if(mkdirsSync(path.dirname(dirname), mode)){
-            fs.mkdirSync(dirname, mode);
-            return true;
-        }
-    }
-}
-
-mkdirsSync('~/srv/anjuke/images', null, function (err) {
-    "use strict";
-    console.log(err);
-});
