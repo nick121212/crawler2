c7b0537b9008c0fc60557978e1e3c6f153e70ae7
nick <nick121212@126.com>
2016-08-12 14:23:15 +0800

    房点评

--- spider/download/request.js
+++ spider/download/request.js
@@ -13,27 +13,33 @@ class Download {
         let defer = Promise.defer();
 
         try {
-            let stream = fs.createWriteStream(`${__dirname}/../../images/${md5(uri.toString())}`);
+            let urlId = md5(uri.toString());
+            let stream = null;
             let req;
 
-            uri = URI(uri.toString());
-            uri = uri.toString().replace(/\d+x\d+\D\.(?:jpg|png)/, "10000x10000m." + uri.suffix());
-
-            req = request.get(uri.toString());
-            if (settings.useProxy && settings.ipInfo && settings.ipInfo.port && settings.ipInfo.port) {
-                req.proxy(`http://${settings.ipInfo.host}:${settings.ipInfo.port}`);
-            }
-            req.on('response', function (res) {
-                console.log(res.status);
-            });
-            req.on('error', function (err) {
-                defer.reject(err);
-            });
-            stream.on('finish', function () {
-                console.log(`fetch picture complete ${uri.toString()} at ${new Date()}`);
+            if (fs.existsSync(`${__dirname}/../../images/${urlId}`)) {
                 defer.resolve();
-            });
-            req.pipe(stream);
+            } else {
+                stream = fs.createWriteStream(`${__dirname}/../../images/${urlId}`);
+                uri = URI(uri.toString());
+                uri = uri.toString().replace(/\d+x\d+\D\.(?:jpg|png)/, "10000x10000m." + uri.suffix());
+
+                req = request.get(uri.toString());
+                if (settings.useProxy && settings.ipInfo && settings.ipInfo.port && settings.ipInfo.port) {
+                    req.proxy(`http://${settings.ipInfo.host}:${settings.ipInfo.port}`);
+                }
+                req.on('response', function (res) {
+                    console.log(res.status);
+                });
+                req.on('error', function (err) {
+                    defer.reject(err);
+                });
+                stream.on('finish', function () {
+                    console.log(`fetch picture complete ${uri.toString()} at ${new Date()}`);
+                    defer.resolve();
+                });
+                req.pipe(stream);
+            }
         } catch (err) {
             defer.reject(err);
         }
@@ -42,6 +48,6 @@ class Download {
     }
 }
 
-module.exports = (app, core)=> {
+module.exports = ()=> {
     return new Download();
 };
