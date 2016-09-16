697e96911c6bd99dc92c97b344adbd6e0c2db94e
nick <nick121212@126.com>
2016-09-16 17:55:54 +0800

    房点评

--- tools/config/zhongyuan/index.js
+++ tools/config/zhongyuan/index.js
@@ -17,7 +17,7 @@ module.exports = exports = (core) => {
     };
     // 白名单
     // config.addWhitePath(/^\/ershoufang(?:\/\D+|\/.+\.html|)(?:|\/g\d+)\/?$/);
-
+    // 中原经纪人
     config.addWhitePath(/^\/xiaoqu\/g\d+\/?$/);
 
     // -----------------页面配置------------------
