fe342f4dd3adb284d6a3b4921bfa7ad6786a6143
nick <nick121212@126.com>
2016-10-13 16:27:52 +0800

    增加了error文件

---
+++ spider/socket/retryqueue.js
@@ -0,0 +1,69 @@
+/**
+ * Created by NICK on 16/7/1.
+ */
+module.exports = exports = (app, core, socket) => {
+    socket.on("crawler:retryqueue", (params, cb)=> {
+        let config = params.key;
+        let index = `${config.key}-crawler-allin`;
+        let type = 'mqurls';
+        let total = 0;
+        let queueEs = new app.spider.lib.queue_store_es(config.key);
+
+        let getMoreUntilDone = (err, response)=> {
+            let promises = [];
+
+            if (err) {
+                return cb({
+                    ret: -1,
+                    message: err.message
+                });
+            }
+            response.hits.hits.forEach(function (res) {
+                res = res._source;
+                promises.push(queueEs.addQueueItemsToQueue(res, config.key));
+            });
+            total += response.hits.hits.length;
+            app.spider.socket.log({
+                message: `当前scroll：{${response._scroll_id}},数量：${total},总数量：${response.hits.total}`,
+                isError: false
+            });
+            if (response.hits.total !== total) {
+                Promise.all(promises).then(()=> {
+                    core.elastic.scroll({
+                        scrollId: response._scroll_id,
+                        scroll: '30s'
+                    }, getMoreUntilDone);
+                }, (err)=> {
+                    return cb({
+                        ret: -1,
+                        message: err.message
+                    });
+                });
+            } else {
+                cb({
+                    ret: 0
+                });
+            }
+        };
+
+
+        Promise.all([
+            core.q.deleteQueue(`crawler.urls.${config.key}`),
+        ]).then(() => {
+            return core.elastic.search({
+                index: index,
+                type: type,
+                scroll: '10s',
+                search_type: 'scan',
+                // _source: ["urlId", "url"],
+                from: 0,
+                size: 100
+            }, getMoreUntilDone);
+        }, (e)=> {
+            cb({
+                ret: -1,
+                msg: e.message
+            });
+        });
+    });
+};
